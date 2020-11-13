85906f2638e96bc07c80da96239e137fd812165e
NestorMonroy <nestor.monroy.90@gmail.com>
2020-11-12 19:37:56 -0600

    change register and login views

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,6 +1,7 @@
 from django.http import request
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
+from django.views.generic import CreateView, FormView
 from django.contrib.auth import get_user_model, authenticate, login
 from django.utils.http import is_safe_url
 
@@ -25,22 +26,24 @@ def guest_login_page(request):
             return redirect(redirect_path)
         else:
             return redirect("/register/")
-        
+
     return redirect("/register/")
 
 
-def login_page(request):
-    form = LoginForm(request.POST or None)
-    context = {
-        "form": form
-    }
-    next_ = request.GET.get('next')
-    next_post = request.POST.get('next')
-    redirect_path = next_ or next_post or None
-    if form.is_valid():
-        username  = form.cleaned_data.get("username")
-        password  = form.cleaned_data.get("password")
-        user = authenticate(request, username=username, password=password)
+class LoginView(FormView):
+    form_class = LoginForm
+    template_name = 'accounts/login.html'
+    success_url = '/'
+
+    def form_valid(self, form):
+        request = self.request
+        next_ = request.GET.get('next')
+        next_post = request.POST.get('next')
+        redirect_path = next_ or next_post or None
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(request, username=email, password=password)
+        print(user)
         if user is not None:
             login(request, user)
             try:
@@ -51,18 +54,10 @@ def login_page(request):
                 return redirect(redirect_path)
             else:
                 return redirect("/")
-        else:
-            # Return an 'invalid login' error message.
-            print("Error")
-    return render(request, "accounts/login.html", context)
+        return super(LoginView, self).form_valid(form)
 
 
-
-def register_page(request):
-    form = RegisterForm(request.POST or None)
-    context = {
-        "form": form
-    }
-    if form.is_valid():
-        form.save()
-    return render(request, "accounts/register.html", context)
+class RegisterView(CreateView):
+    form_class = RegisterForm
+    template_name = 'accounts/register.html'
+    success_url = '/login/'
--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -6,7 +6,7 @@ from django.urls import path
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LogoutView
 
-from accounts.views import login_page, register_page, guest_login_page
+from accounts.views import LoginView, RegisterView, guest_login_page
 from addresses.views import checkout_address_create_view, checkout_address_reuse_view
 from carts.views import cart_detail_api_view
 from .views import home_page, about_page, contact_page
@@ -20,10 +20,10 @@ urlpatterns = [
     url(r'^search/', include(('search.urls', 'search'), namespace='search')),
     url(r'^cart/', include(('carts.urls', 'cart'), namespace='cart')),
     url(r'^api/cart/$', cart_detail_api_view, name='api-cart'),
-    url(r'^login/$', login_page, name='login'),
+    url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^checkout/address/create/$', checkout_address_create_view, name='checkout_address_create'),
     url(r'^checkout/address/reuse/$', checkout_address_reuse_view, name='checkout_address_reuse'),
-
+    url(r'^register/$', RegisterView.as_view(), name='register'),
     url(r'^register/guest/$', guest_login_page, name='guest_register'),
 
 
